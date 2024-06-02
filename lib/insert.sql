4b4b86befa326194cd1cc61a485eb9cc3943d884
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-02 14:49:49 +0300

    py

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color) VALUES ('Mr. Chocolate', 5, 'M', 'brown');
+
+INSERT INTO bears (name, age, sex, color) VALUES ('Rowdy', 3, 'M', 'black');
+
+INSERT INTO bears (name, age, sex, color) VALUES ('Tabitha', 4, 'F', 'white');
+
+INSERT INTO bears (name, age, sex, color) VALUES ('Sergeant Brown', 6, 'M', 'brown');
+
+INSERT INTO bears (name, age, sex, color) VALUES ('Melissa', 2, 'F', 'black');
+
+INSERT INTO bears (name, age, sex, color) VALUES ('Grinch', 7, 'M', 'green');
+
+INSERT INTO bears (name, age, sex, color) VALUES ('Wendy', 3, 'F', 'brown');
+
+INSERT INTO bears (name, age, sex, color) VALUES (NULL, 1, 'M', 'black');
